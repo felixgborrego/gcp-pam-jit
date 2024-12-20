7d250b7269dc9acada1cc89d7edc42bf6cd19cec
Steve Goodenough <steve.goodenough@johnlewis.co.uk>
2024-12-20 13:00:04 +0000

    handle getting users email

--- pkg/gcp/gcp.go
+++ pkg/gcp/gcp.go
@@ -4,42 +4,46 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"os/exec"
-	"strings"
 
 	"golang.org/x/oauth2/google"
+	"google.golang.org/api/oauth2/v2"
+	"google.golang.org/api/option"
 )
 
 // get user email from service account and if that fails then fall back to local email held for gcloud
 func GetUserEmail() (email string, err error) {
+	// Initialize a context
 	ctx := context.Background()
-	credential, err := google.FindDefaultCredentials(ctx)
+
+	// Use default credentials
+	creds, err := google.FindDefaultCredentials(ctx, oauth2.UserinfoEmailScope)
 	if err != nil {
-		return "", fmt.Errorf("error getting default credentials: %e", err)
+		return "", fmt.Errorf("failed to obtain default credentials: %e", err)
 	}
 
-	content := map[string]interface{}{}
-
-	json.Unmarshal(credential.JSON, &content)
-
-	if content["client_email"] != nil {
-		return content["client_email"].(string), nil
-	} else {
-		email, err := getGcloudUserEmail()
-		if err != nil {
-			return "", fmt.Errorf("error getting user email: %e", err)
+	// Check if credentials are from a service account
+	if len(creds.JSON) > 0 {
+		var content map[string]interface{}
+		if err := json.Unmarshal(creds.JSON, &content); err == nil {
+			if email, ok := content["client_email"].(string); ok {
+				return email, nil
+			}
 		}
-		return email, nil
 	}
-}
 
-func getGcloudUserEmail() (string, error) {
-	cmd := exec.Command("gcloud", "auth", "list", "--format=value(account)")
-	output, err := cmd.Output()
+	// If not, then the credentials are from a user account
+
+	// Create an OAuth2 service using the credentials
+	oauth2Service, err := oauth2.NewService(ctx, option.WithTokenSource(creds.TokenSource))
 	if err != nil {
-		return "", fmt.Errorf("error executing gcloud command: %w", err)
+		return "", fmt.Errorf("unable to create OAuth2 service: %e", err)
 	}
 
-	email := strings.TrimSpace(string(output))
-	return email, nil
+	// Get the authenticated user's email address
+	userInfo, err := oauth2Service.Userinfo.Get().Do()
+	if err != nil {
+		return "", fmt.Errorf("unable to retrieve user info: %e", err)
+	}
+
+	return userInfo.Email, nil
 }
--- pkg/slack/slack.go
+++ pkg/slack/slack.go
@@ -4,28 +4,25 @@ import (
 	"fmt"
 
 	"github.com/felixgborrego/gpc-pam-jit/pkg/config"
-	"github.com/felixgborrego/gpc-pam-jit/pkg/pamjit"
 	"github.com/felixgborrego/gpc-pam-jit/pkg/gcp"
+	"github.com/felixgborrego/gpc-pam-jit/pkg/pamjit"
 	"github.com/slack-go/slack"
 )
 
 func SendSlackMessage(cfg *config.Config, options *pamjit.RequestOptions, link string) error {
 	api := slack.New(cfg.Slack.Token)
-	email, _ := gcp.GetUserEmail()
+	email, err := gcp.GetUserEmail()
+	if email == "" {
+		fmt.Printf("Unable to retrieve your email but will send the request to Slack anyway\n%s\n", err)
+	}
 
 	// Use Slack Block Kit for better formatting
 	blocks := []slack.Block{
 		slack.NewHeaderBlock(
 			slack.NewTextBlockObject("plain_text", ":lock: PAM Request", false, false),
 		),
-		// wait for voting on format...
-		// slack.NewSectionBlock(
-		// 	slack.NewTextBlockObject("mrkdwn", fmt.Sprintf("*Entitlement:* `%s`\n*Resource:* `%s`\n*Requested by:* `%s`\n*Duration:* `%s`\n*Justification:* `%s`", options.EntitlementID, options.ProjectID, email, options.Duration, options.Justification), false, false),
-		// 	nil,
-		// 	nil,
-		// ),
 		slack.NewSectionBlock(
-			slack.NewTextBlockObject("mrkdwn", fmt.Sprintf("*%s* has requested *%s* on resource *%s* for *%s*, with the justification\n>%s", email, options.EntitlementID, options.ProjectID, options.Duration, options.Justification), false, false),
+			slack.NewTextBlockObject("mrkdwn", fmt.Sprintf("Entitlement: *%s*\nResource: *%s*\nRequested by: *%s*\nDuration: *%s*\nJustification: *%s*", options.EntitlementID, options.ProjectID, email, options.Duration, options.Justification), false, false),
 			nil,
 			nil,
 		),
@@ -37,7 +34,7 @@ func SendSlackMessage(cfg *config.Config, options *pamjit.RequestOptions, link s
 	}
 
 	// send the message with blocks
-	_, _, err := api.PostMessage(cfg.Slack.Channel, slack.MsgOptionBlocks(blocks...))
+	_, _, err = api.PostMessage(cfg.Slack.Channel, slack.MsgOptionBlocks(blocks...))
 	if err != nil {
 		return fmt.Errorf("error sending message to Slack: %e", err)
 	}
